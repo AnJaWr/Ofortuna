5f2186e25e65b94f23b8154ce72dafca2b8876c6
anjawr <48285666+AnJaWr@users.noreply.github.com>
2019-08-02 18:25:01 +0200

    Add files via upload

---
+++ precache-manifest.2d056712a24d421a2e17894b68a25ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b91d14f8c7ad4f9aceab9b3120956a",
+    "url": "/Ofortuna/index.html"
+  },
+  {
+    "revision": "e4a39424ba7054140dd0",
+    "url": "/Ofortuna/static/css/main.51c42385.chunk.css"
+  },
+  {
+    "revision": "8a8bb3248b5bda8acde4",
+    "url": "/Ofortuna/static/js/2.550e3f58.chunk.js"
+  },
+  {
+    "revision": "e4a39424ba7054140dd0",
+    "url": "/Ofortuna/static/js/main.4d8c4f7c.chunk.js"
+  },
+  {
+    "revision": "5f8aabf03118bde9800d",
+    "url": "/Ofortuna/static/js/runtime~main.c40347ad.js"
+  },
+  {
+    "revision": "3306eb12c16d21d5a4138c993b637fc3",
+    "url": "/Ofortuna/static/media/School-Wallpapers-6.3306eb12.jpg"
+  }
+]);
